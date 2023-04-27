a9ecf6201f7f76c1a853bd176f9db496595f2a8d
Noor Asmar <noor_jb@outlook.com>
2023-04-27 10:31:50 +0300

    add set of fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import { BsFill0SquareFill } from "react-icons/bs";
 
 function App() {
   return (
