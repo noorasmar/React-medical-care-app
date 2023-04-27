d7ba2d9ccb42497d040d325c9d890729d8c11edf
Noor Asmar <noor_jb@outlook.com>
2023-04-27 10:03:15 +0300

    add bootstrap + react icon packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import { BsFill0SquareFill } from "react-icons/bs";
 
 function App() {
   return (
     <div className="App">
+      <BsFill0SquareFill />
+      <i className="bi bi-0-square-fill"></i>
+
     </div>
   );
 }
