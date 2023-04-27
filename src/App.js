07f5b96eabcdb618a73b4908b985863fc4d41c24
Noor Asmar <noor_jb@outlook.com>
2023-04-27 10:18:08 +0300

    add set of color's

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import { BsFill0SquareFill } from "react-icons/bs";
 function App() {
   return (
     <div className="App">
-      <BsFill0SquareFill />
-      <i className="bi bi-0-square-fill"></i>
-
     </div>
   );
 }
