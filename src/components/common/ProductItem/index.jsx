1435ca2b53b8009844e953fd3194611b005d1e92
Noor Asmar <noor_jb@outlook.com>
2023-05-02 11:36:19 +0300

    add un loves functionallity

--- src/components/common/ProductItem/index.jsx
+++ src/components/common/ProductItem/index.jsx
@@ -1,13 +1,20 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import styles from './styles.module.css';
 import LovesContext from '../../../contexts/LovesContext';
 
 function ProductItem({title, category, imgSrc, price}) {
     const { setLoves } = useContext(LovesContext);
+    const [isLoved, setIsLoved] = useState(false)
 
-    const handleClick = () => {
+    const handleLove = () => {
         setLoves(prevLoves => prevLoves + 1);
+        setIsLoved(!isLoved)
     };
+    const handleUnLove = () => {
+        setLoves(prevLoves => prevLoves > 0 ? prevLoves - 1 : 0);
+        setIsLoved(!isLoved)
+    };
+
     return (
         <div className={styles.item}>
             <h5 className='raleway'>
@@ -23,7 +30,12 @@ function ProductItem({title, category, imgSrc, price}) {
                 </h5>
                 <div className={styles.tools}>
                     <i className="bi bi-cart-plus"></i>
-                    <i className="bi bi-heart" onClick={handleClick}></i>
+                    {
+                        !isLoved ? 
+                            <i className="bi bi-heart" onClick={handleLove}></i> 
+                            : 
+                            <i className="bi bi-heart-fill" style={{color: 'var(--red-color)'}} onClick={handleUnLove}></i>
+                    }
                 </div>
             </div>
         </div>
