c25709f63be5f7598449b110ac88d444a84b0d6d
Noor Asmar <noor_jb@outlook.com>
2023-05-03 12:38:00 +0300

    fix ui dashboard

--- src/components/common/Table/index.jsx
+++ src/components/common/Table/index.jsx
@@ -136,7 +136,7 @@ function Table() {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalLabel2">Add Product</h1>
+                            <h1 className="modal-title fs-5" id="exampleModalLabel2">Update Product</h1>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <form  onSubmit={handleUpdate}>
